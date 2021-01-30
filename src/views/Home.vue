551b8764eff9738327362ff2a16491bd3bd2fd58
mac <mac@NickdeMacBook-Pro.local>
2021-01-30 23:06:58 +0800

    ceshi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ name }}
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -11,6 +13,11 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      name: 'sfe'
+    }
+  },
   components: {
     HelloWorld
   }
