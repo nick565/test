66b45f24f29b151c60f7efdc6313ef5d42cd0d1c
nick565 <nick_w2@163.com>
2021-01-31 00:37:00 +0800

    111

---
+++ src/views/Test11.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="home">
+    {{ name }}
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name: '版本1'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
